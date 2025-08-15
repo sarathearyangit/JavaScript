f47b51591e8b81ea999c66971d4d8f75c3c9f5ae
Aryan Sarathe <sarathearyan731@gmail.com>
2025-08-15 17:28:41 +0000

    conversionoperation

---
+++ 01basics/3conope.js
@@ -0,0 +1,46 @@
+let score="33ab"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNum=Number(score)
+console.log(typeof valueInNum)
+console.log(valueInNum)//NaN not a no.
+/*"33"=33
+"33ab"=NaN
+true=1;false=0;
+*/
+
+let value=null
+
+console.log(typeof value)
+console.log(typeof (value))
+
+let valueInVal=Number(value)
+console.log(typeof valueInVal)
+console.log(valueInVal)
+
+
+let dm=undefined
+
+console.log(typeof dm)
+console.log(typeof (dm))
+
+let valueIndm=Number(dm)
+console.log(typeof valueIndm)
+console.log(valueIndm)
+
+
+let islog=true
+
+let boolislog=Boolean(islog)
+console.log(boolislog)
+/*1=true 0=false
+""=false  "aryan"=true*/
+
+
+let no=33
+
+let strval=String(no)
+console.log(strval);
+console.log(typeof strval);
