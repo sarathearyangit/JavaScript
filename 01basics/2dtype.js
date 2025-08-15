a940a105dad2cfb20a7f67f3502353d7fb126224
Aryan Sarathe <sarathearyan731@gmail.com>
2025-08-15 15:33:48 +0000

    dtype done

---
+++ 01basics/2dtype.js
@@ -0,0 +1,28 @@
+"use strict"//
+
+//alert(3+3) we are using nodejs,not browser
+
+console.log(3+3)
+
+console.log("aryan")
+
+let name="aryan"
+let age=5
+let getLogged=true
+
+/*p.d.t
+no.=2pow 52
+bigInt
+string=""
+boolean=true/false
+null=standlone value
+undefined=
+symbol=unique
+*/
+
+/*n.p.d.t
+object
+*/
+console.log(typeof age)//no
+console.log(typeof null)//
+console.log(typeof undefined)//undef
