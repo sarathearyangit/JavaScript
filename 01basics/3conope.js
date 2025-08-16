d69fa966a01f391ba405c81d9cd1396fe0ef9eaf
Aryan Sarathe <sarathearyan731@gmail.com>
2025-08-16 13:56:07 +0000

    conversionoperation done

--- 01basics/3conope.js
+++ 01basics/3conope.js
@@ -6,10 +6,9 @@ console.log(typeof (score))
 let valueInNum=Number(score)
 console.log(typeof valueInNum)
 console.log(valueInNum)//NaN not a no.
-/*"33"=33
-"33ab"=NaN
-true=1;false=0;
-*/
+//"33"=33
+//"33ab"=NaN
+//true=1;false=0;
 
 let value=null
 
@@ -35,8 +34,8 @@ let islog=true
 
 let boolislog=Boolean(islog)
 console.log(boolislog)
-/*1=true 0=false
-""=false  "aryan"=true*/
+//1=true 0=false
+//""=false  "aryan"=true
 
 
 let no=33
@@ -44,3 +43,40 @@ let no=33
 let strval=String(no)
 console.log(strval);
 console.log(typeof strval);
+
+
+//..........Operation........
+let val=2
+let negval=-val;
+console.log(negval);
+
+// console.log(5 ** 2);
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="aryan"
+let str2="sarathe"
+
+let str3=str1+str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+4*5%3);//*, /, % (l to r)  + / 
+// console.log((3+4)*5%3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2,num3
+
+num1=num2=num3=2+3
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter);
