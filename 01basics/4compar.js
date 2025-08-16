d6d0ca6cefa245729a91a30b7d67116871abf059
Aryan Sarathe <sarathearyan731@gmail.com>
2025-08-16 16:28:56 +0000

    comparsion done

---
+++ 01basics/4compar.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1); all good
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1); 
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+//kabhi 0 or null inconsistent
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//=== strictly check datatype also
+console.log("2"===2);
